a6459453b51cd608f66ae752ad696f1d99ff92e8
Gevorg Harutyunyan <gevharut@gmail.com>
2023-06-26 02:45:01 +0400

    Header improvement
    Signed-off-by: Gevorg Harutyunyan <gevharut@gmail.com>

--- src/app/_components/TheHeader/TheHeader.tsx
+++ src/app/_components/TheHeader/TheHeader.tsx
@@ -5,7 +5,7 @@ const TheHeader: React.FC = () => {
   return (
     <header
       className="
-        p-2 px-4
+        p-4 px-10
         top-0 left-0 right-0
         fixed
         bg-white bg-opacity-70
@@ -16,8 +16,11 @@ const TheHeader: React.FC = () => {
         flex flex-row justify-between items-center
       "
     >
-      <Link href={'/'}>
-        <div className="flex flex-row gap-2 align-middle">
+      <Link href={'/'} className="group">
+        <div className="flex flex-row gap-4 align-middle justify-center items-center">
+          <span className=" text-4xl text-zinc-200 group-hover:-translate-x-1 transition  group-hover:text-pink-400">
+            &#123;
+          </span>
           <Image
             src="/profilePic.png"
             width={40}
@@ -25,11 +28,28 @@ const TheHeader: React.FC = () => {
             className="w-10 h-10 aspect-auto rounded-full shadow-sm"
             alt="Gevorg Harutyunyan profile picture"
           />
-          <div>
-            {/* <h2 className="text-lg leading-5">gh.am</h2> */}
-            {/* <h3 className="text-xs text-slate-400">
-              Front End Engineer, Full-Stack Developer
-            </h3> */}
+          <div className="flex flex-row gap-2 items-center">
+            <h2
+              className="
+                mr-1.5
+                text-2xl font-bold
+                tracking-tight
+
+              "
+            >
+              <span
+                className="
+                text-pink-400
+                  delay-150 ease-in-out
+                "
+              >
+                gh
+              </span>
+              <span className="text-zinc-300">.am</span>
+            </h2>
+            <span className="text-4xl text-zinc-200 group-hover:translate-x-1 transition  group-hover:text-pink-400">
+              &#125;
+            </span>
           </div>
         </div>
       </Link>
